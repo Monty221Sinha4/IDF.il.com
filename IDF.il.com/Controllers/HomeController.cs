1e9c924c73f94c77434d395ca749526442261f86
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2022-03-01 19:51:39 +0000

    I have added the images and home controller to the site

--- IDF.il.com/Controllers/HomeController.cs
+++ IDF.il.com/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace IDF.il.com.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Home()
         {
             return View();
         }
